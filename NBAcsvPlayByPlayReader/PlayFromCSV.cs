254d89dc4237f4ed568b30451fbc233091226661
Michał Salamon <ms055452@student.ath.edu.pl>
2020-07-23 10:29:29 +0200

    Late initial commit

---
+++ NBAcsvPlayByPlayReader/PlayFromCSV.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBAcsvPlayByPlayReader
+{
+    public class PlayFromCSV
+    {
+        public string URL { get; set; }
+        public string GameType { get; set; }
+        public string Location { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string WinningTeam { get; set; }
+        public int Quarter { get; set; }
+        public int SecLeft { get; set; }
+        public string AwayTeam { get; set; }
+        public string AwayPlay { get; set; }
+        public string AwayScore { get; set; }
+        public string HomeTeam { get; set; }
+        public string HomePlay { get; set; }
+        public string HomeScore { get; set; }
+        public string Shooter { get; set; }
+        public string ShotType { get; set; }
+        public string ShotOutcome { get; set; }
+        public int ShotDist { get; set; }
+        public string Assister { get; set; }
+        public string Blocker { get; set; }
+        public string FoulType { get; set; }
+        public string Fouler { get; set; }
+        public string Fouled { get; set; }
+        public string Rebounder { get; set; }
+        public string ReboundType { get; set; }
+        public string ViolationPlayer { get; set; }
+        public string ViolationType { get; set; }
+        public string TimeoutTeam { get; set; }
+        public string FreeThrowShooter { get; set; }
+        public string FreeThrowOutcome { get; set; }
+        public string FreeThrowNum { get; set; }
+        public string EnterGame { get; set; }
+        public string LeaveGame { get; set; }
+        public string TurnoverPlayer { get; set; }
+        public string TurnoverType { get; set; }
+        public string TurnoverCause { get; set; }
+        public string TurnoverCauser { get; set; }
+        public string JumpballAwayPlayer { get; set; }
+        public string JumpballHomePlayer { get; set; }
+        public string JumpballPoss { get; set; }
+
+    }
+}
---
+++ NBAcsvPlayByPlayReader/PlaysCSVReader.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace NBAcsvPlayByPlayReader
+{
+    public static class PlaysCSVReader
+    {
+        public static IEnumerable<PlayFromCSV> GetPlays(int year)
+        {
+            using var reader = new StreamReader($"NBA-PBP_{year}-{year + 1}.csv");
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            return csv.GetRecords<PlayFromCSV>();
+        }
+    }
+}
