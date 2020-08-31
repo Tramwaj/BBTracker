988161d6db9943e44ad66e0f9696678355b15ebf
Michał Salamon <ms055452@student.ath.edu.pl>
2020-08-31 12:37:17 +0200

    add serialization as alternative data source

---
+++ BBTracker/DB/Serializer.cs
@@ -0,0 +1,137 @@
+﻿using BBTracker.Models;
+using BBTracker.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace BBTracker.DB
+{
+    public class Serializer : IDataAccess
+    {
+        private BinaryFormatter _bin;
+        private List<Game> _games;
+        private List<Player> _players;
+        private List<Play> _plays;
+        private List<PlayerGame> _playerGames;    
+
+
+        public Serializer()
+        {
+            _bin = new BinaryFormatter();
+            CreateFilesIfFirstRun();
+            LoadData();
+        }
+        private void CreateFilesIfFirstRun()
+        {
+            var _g = new List<Game>();
+            var _py = new List<Play>();
+            var _pr = new List<Player>();
+            var _pg = new List<PlayerGame>();
+            Serialize("games.bin", _g);
+            Serialize("plays.bin", _py);
+            Serialize("players.bin", _pr);
+            Serialize("playergames.bin", _pg);
+        }
+        private void LoadData()
+        {
+            _games = Deserialize<List<Game>>("games.bin");
+            _players = Deserialize<List<Player>>("players.bin");
+            _plays = Deserialize<List<Play>>("plays.bin");
+            _playerGames = Deserialize<List<PlayerGame>>("playergames.bin");
+        }
+
+
+        public Task<List<Game>> GetAllGamesAsync()
+        {
+            return Task.Run(() => { return _games; });
+        }
+
+        public Task<List<Play>> GetPlaysFromGameAsync(int gameId)
+        {
+            return Task.Run(() => { return _plays.Where(p => p.Game.Id == gameId).ToList(); });
+        }
+
+        public Task<List<Play>> GetPlayerPlaysAsync(int playerId)
+        {
+            return Task.Run(() => { return _plays.Where(p => p.Player.Id == playerId).ToList(); });
+        }
+
+        public void OpenConnection()
+        {            
+        }
+
+        public void CloseConnectionAsync()
+        {
+        }
+
+        public void AddGameAsync(Game game)
+        {
+            _games.Add(game);
+        }
+
+        public void AddPlaysAsync(ICollection<Play> plays)
+        {
+            _plays.AddRange(plays);
+        }
+
+        public void AddPlayerGames(ICollection<PlayerGame> playerGames)
+        {
+            _playerGames.AddRange(playerGames);
+        }
+
+        public void SaveChangesAsync()
+        {
+            Serialize("games.bin", _games);
+            Serialize("plays.bin", _plays);
+            Serialize("players.bin",_players);
+            Serialize("playergames.bin", _playerGames);
+
+        }
+
+        public Task<List<Player>> GetPlayersAsyncAlreadyConnected()
+        {
+            return Task.Run(() => { return _players; });
+        }
+        private void Serialize(string file, object objToSerialize)
+        {
+            using (var stream =  File.Open(file, FileMode.Create))
+            {
+                try
+                {
+                    _bin.Serialize(stream, objToSerialize);
+                }
+                catch (SerializationException e)
+                {
+                    Console.WriteLine("Serialization fail. Reason: " + e.Message);
+                    throw;
+                }
+            }
+        }
+        
+        private T Deserialize<T>(string file)
+        {
+            T items;
+
+            using (Stream stream = File.Open(file, FileMode.Open))
+            {
+                try
+                {
+                    items = (T)_bin.Deserialize(stream);
+                }
+                catch (SerializationException e)
+                {
+                    Console.WriteLine("deserialization failed. Reason: " + e.Message);
+                    throw;
+                }
+            }
+            return items;
+        }
+    }
+}
--- BBTracker/Models/PlayFactory.cs
+++ BBTracker/Models/PlayFactory.cs
@@ -1,60 +1,130 @@
-﻿using System;
+﻿//using Microsoft.EntityFrameworkCore.Internal;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Navigation;
+using System.Linq;
 
 namespace BBTracker.Models
 {
     public class PlayFactory
     {
+        private List<Play> _plays;
         private Play _play;
-        private Play _resultingPlay;
-        private Game _game;
+        private readonly Game _game;
+        public bool HasPlay => _plays.Any();
 
         public PlayFactory(Game game)
         {
             _game = game;
-        }
-        
-
-        public void CreateNewPlay(Player player, bool teamB)
-        {
-            if (_play is null)
-            {
-                _play = new Play
-                {
-                    Player = player,
-                    TeamB = teamB,
-                    Date = DateTime.Now.Date,
-                    GameTime = DateTime.Now - _game.Start,
-                    Game = _game
-                };
-            }
-            else
-            {
-                _resultingPlay.Player = player;           _resultingPlay.TeamB = teamB; //do obadania
-            }
+            _play = new Play();
+            _plays = new List<Play>();
         }
 
-        public void ResultingPlay(PlayType playType, bool sameTeam)
+
+        public void SelectPlayer(Player player, bool teamB)
         {
-            _resultingPlay = new Play
+            _play.Player = player;
+            if (this.HasPlay) SelectSecondPlayer(player, teamB);
+
+            _play.TeamB = teamB;
+            _play.Date = DateTime.Now.Date;
+            _play.GameTime = DateTime.Now - _game.Start;
+            _play.Game = _game;
+        }
+
+        private void SelectSecondPlayer(Player player, bool teamB)
+        {
+            _play.Player = player;
+            _play.TeamB = teamB;
+            _plays.Add(_play);
+        }
+
+        public void Cancel()
+        {
+            _play = new Play();
+            _plays = new List<Play>();
+        }
+
+        public void Clear()
+        {
+            Cancel();
+        }
+
+
+        public void ChoosePlayType(PlayType playType)
+        {
+            _play.PlayType = playType;
+            switch (playType)
             {
-                PlayType = playType,
+                case PlayType.CheckOut:
+                    _plays.Add(_play);
+                    _play = CopyPlay();
+                    _play.PlayType = PlayType.CheckIn;
+                    break;
+                case PlayType.TurnOver:
+                    _plays.Add(_play);
+                    _play = CopyPlay();
+                    _play.PlayType = PlayType.Steal;
+                    break;
+                case PlayType.CheckIn:
+                    _plays.Add(_play);
+                    break;
+            }
+        }
+        private Play CopyPlay()
+        {
+            return new Play
+            {
                 Date = _play.Date,
+                Game = _play.Game,
                 GameTime = _play.GameTime,
-                Points = _play.Points != 0 ?
-                    _play.Points : 0,
-                TeamB = sameTeam ?
-                    _play.TeamB : !_play.TeamB
+                Points = _play.Points
             };
         }
-       // public void ChoosePlaytype(PlayType playType)
+
+
+        /// <summary>
+        /// takes three values: "make","miss","block"
+        /// </summary>
+        /// <param name="result"></param>
+        public void FGResult(string result)
+        {
+            if (_play.PlayType != PlayType.FieldGoal)
+                throw new InvalidOperationException("Play is not a Field goal");
+            switch (result)
+            {
+                case "make":
+                    _play.MadeFG = true;
+                    _plays.Add(_play);
+                    _play = CopyPlay();
+                    _play.PlayType = PlayType.Assist;
+                    break;
+                case "miss":
+                    _play.MadeFG = false;
+                    _plays.Add(_play);
+                    _play = CopyPlay();
+                    _play.PlayType = PlayType.Rebound;
+                    break;
+                case "block":
+                    _play.MadeFG = false;
+                    _play.BlockedFG = true;
+                    _plays.Add(_play);
+                    _play = CopyPlay();
+                    _play.PlayType = PlayType.Block;
+                    break;
+            }
+        }
+
+        public void SelectPoints(int points)
+        {
+            _play.Points = points;
+        }
+
+        // public void ChoosePlaytype(PlayType playType)
         public ICollection<Play> GetPlays()
         {
-            return _resultingPlay == null ?
-                new List<Play> { _play } :
-                new List<Play>{_play, _resultingPlay
-                    };
+            return _plays;
         }
     }
 }
